7a6a7228cffcbc4b7895524b5e84f0c514ba47f1
David Rogers <david@ethos-development.com>
2010-04-06 22:19:02 -0400

    I'm using example_Class in Phake_LoaderTest to test Phake_Loader::load(), so I'd better include my examples.

---
+++ example/Class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author David Rogers
+ * @package example
+ * @category Test_Examples
+ */
+
+/**
+ * @author David Rogers
+ * @package example
+ * @category Test_Examples
+ */
+class example_Class { }
