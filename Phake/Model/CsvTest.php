4b78fb6af45a829ce111cae4e4237861e0a70eff
David Rogers <david@ethos-development.com>
2010-04-08 17:48:23 -0400

    Had to "unbreak" the tests from the last commit... Also adding the "complex.csv" file to the dataset in Phake_Model_CsvTest

--- Phake/Model/CsvTest.php
+++ Phake/Model/CsvTest.php
@@ -1,5 +1,17 @@
 <?php
+/**
+ * @author David Rogers <david@ethos-development.com>
+ * @package Phake_Model
+ * @category Test_Cases
+ */
 
+PHPUnit_Util_Filter::addFileToFilter(__FILE__);
+
+/**
+ * @author David Rogers <david@ethos-development.com>
+ * @package Phake_Model
+ * @category Test_Cases
+ */
 class Phake_Model_CsvTest
 extends PHPUnit_Framework_TestCase
 {
@@ -33,24 +45,45 @@ extends PHPUnit_Framework_TestCase
     } // END test_toArray
 
 
-    public function test_load ( )
+    public static function provide_load ( )
+    {
+        return array(
+            'no filename should generate an Exception' => array( ),
+            'the "simple" file' => array( 'example/simple.csv', array(
+                array( 'simple' => 'data' ),
+            )), // END dataset
+            'the "complex" file' => array( 'example/complex.csv', array(
+                0 => array(
+                    'field' => 'value',
+                    'long-field' => "really long value with\nline breaks and\neverything.",
+                    'after-long-field' => 'value after long field',
+                ), // END row 0
+            )), // END dataset
+        ); // END array
+    } // END provide_load
+
+    /**
+     * @dataProvider provide_load
+     * @param string $filename to load()
+     * @param boolean|array $expected value of toArray() or FALSE to indicate an Exception
+     */
+    public function test_load ( $filename = 'nothing', $expected = false )
     {
         $this->assertSame(array(), $this->fixture->toArray(),
             'Initially, the $fixture should be "empty".'
         );
 
+        if ( $expected === false )
+        {
+            $this->setExpectedException('Phake_Model_Exception');
+        }
+
         $this->assertSame($this->fixture,
-            $this->fixture->load('example/sample.csv')
+            $this->fixture->load($filename)
         );
 
-        $expected = array(
-            0 => array(
-                'sample' => 'data',
-            ),
-        ); // END $expected
-
-        $this->assertSame(
-            $expected, $this->fixture->toArray()
+        $this->assertSame($expected, $this->fixture->toArray(),
+            'The $fixture should match the $expected array.'
         );
     } // END test_load
 
