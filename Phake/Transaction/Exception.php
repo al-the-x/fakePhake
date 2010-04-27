02f4815b7d581d06ee0255d1e0f3add2f4efb77a
David Rogers <david@ethos-development.com>
2010-04-27 16:16:30 -0400

    Dunno how I missed it, but there was no "appropriate Exception" for the Phake_Transaction package.

---
+++ Phake/Transaction/Exception.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author David Rogers <david@ethos-development.com>
+ * @package Phake_Transaction
+ * @category Exceptions
+ */
+
+/**
+ * @author David Rogers <david@ethos-development.com>
+ * @package Phake_Transaction
+ * @category Exceptions
+ */
+class Phake_Transaction_Exception
+extends Phake_Exception
+{
+} // END Phake_Transaction_Exception
+
